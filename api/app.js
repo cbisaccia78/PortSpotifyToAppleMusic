5039b6e97a2aff93165903d7329fc4b88f7bfba3
cbisaccia78 <FEFQ9F1qaz0>
2023-03-29 09:07:11 -0400

    copying examples over from api documentation

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const SpotifyWebApi = require('spotify-web-api-node');
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -15,14 +16,48 @@ fs.readFile('clientSecret,txt', function(err, data) {
   clientSecret = data;
 });
 
-var SpotifyWebApi = require('spotify-web-api-node');
-
-// credentials are optional
+//spotifyApi.setAccessToken('<your_access_token>');
 var spotifyApi = new SpotifyWebApi({
   clientId: clientId,
   clientSecret: clientSecret,
   redirectUri: 'http://localhost'
 });
+
+// Get a user
+spotifyApi.getUser('petteralexis')
+  .then(function(data) {
+    console.log('Some information about this user', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+// Get the authenticated user
+spotifyApi.getMe()
+  .then(function(data) {
+    console.log('Some information about the authenticated user', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+/*
+ * Playlist methods
+ */
+
+// Get a playlist
+spotifyApi.getPlaylist('5ieJqeLJjjI8iJWaxeBLuK')
+  .then(function(data) {
+    console.log('Some information about this playlist', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+// Get a user's playlists
+spotifyApi.getUserPlaylists('thelinmichael')
+  .then(function(data) {
+    console.log('Retrieved playlists', data.body);
+  },function(err) {
+    console.log('Something went wrong!', err);
+  });
 
 
 /*
